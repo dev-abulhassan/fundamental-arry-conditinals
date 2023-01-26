4d7e3ca639ff6aeca65e9cf10ea2ff851104989c
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-27 04:09:23 +0600

    array

---
+++ array.js
@@ -0,0 +1,8 @@
+let booksName = ["How To Talk To Anyone", "Atomic Habits", "JavaScript"];
+let mobileName = ["KG-TEL", "Geo Phone", "Vivo", "Symphony"];
+let mobilePrice = [1000, 1200, 2200, 2500];
+
+// array element check
+console.log(mobilePrice.length);
+// index check
+console.log(mobileName[2]);
