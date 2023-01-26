964d1cb852ca279365d10703ba5b1c3f58b2f857
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-27 03:40:22 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,23 @@
+// number type variable
+ var keyboardPrice = 1380;
+
+//  string type variable
+ var keyboardName = "Delux";
+
+//  boolean type variable
+var iAmSingle = false;
+
+// number variable 2type 
+   // integer number 
+   var number = 2;
+   // float or decimal
+   var number2 = 12.32;
+
+// variable naming convention
+   // 1. single word
+   // 2. no gap or space
+   // 3. no quote
+   // 4. no keyword
+   // 5.can not start with number
+   // 6.can not contain dash
+   // 7. used camelCase
