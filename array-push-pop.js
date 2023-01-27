dc5987e015177cf29f52bf152124980a6c3c322c
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-27 18:05:37 +0600

    push & pop

---
+++ array-push-pop.js
@@ -0,0 +1,9 @@
+let numbers = [23, 58, 38,];
+numbers.push(99);
+console.log(numbers);
+
+
+// use pop to get element
+let friendsName = ["masud rana", "masud", "rana", "arif", "monirul"];
+friendsName.pop();
+console.log(friendsName);
