6a0a52f8fbc8a8638c1bdfee71aed5bbe42baaa0
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-27 17:32:48 +0600

    index of array

---
+++ array-index.js
@@ -0,0 +1,12 @@
+let number = [23, 58, 38, 23, 25, 36, 60, 40];
+// 1. get element by index
+// console.log(number[0]);
+
+// 2. set element by index value
+number[2] = 20;
+// console.log(number[2]);
+
+// find index of an element
+// const positionIdex = number.indexOf[36];
+const positionIdex = number.indexOf(36);
+console.log(positionIdex);
