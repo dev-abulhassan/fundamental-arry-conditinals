7afd58d4d1a938db32c77a3d628dd18b6d52339d
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-27 18:15:01 +0600

    array element push abd pop

--- array-push-pop.js
+++ array-push-pop.js
@@ -1,9 +1,18 @@
+// use push to get element
+
 let numbers = [23, 58, 38,];
 numbers.push(99);
+console.log(numbers);
+// unshift used add element by beginning  
+numbers.unshift(55);
 console.log(numbers);
 
 
 // use pop to get element
 let friendsName = ["masud rana", "masud", "rana", "arif", "monirul"];
 friendsName.pop();
+console.log(friendsName);
+
+// shift use to remove element by beginning
+friendsName.shift();
 console.log(friendsName);
